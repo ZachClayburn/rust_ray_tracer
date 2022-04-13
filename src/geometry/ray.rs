e2334b6bea1464e77c9d0f35b356dba65ee52ed1
Zach Clayburn <zachclayburn@gmail.com>
2022-04-13 12:12:01 -0600

    Added ray class

---
+++ src/geometry/ray.rs
@@ -0,0 +1,36 @@
+use super::vec3::{Point3, Vec3};
+
+#[derive(Debug, Default, Clone, Copy)]
+struct Ray {
+    origin: Point3,
+    direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Self { origin, direction }
+    }
+
+    pub fn at(self, t: f64) -> Point3 {
+        self.origin + self.direction * t
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn can_construct_ray() {
+        Ray::new(Vec3::new(1., 2., 3.), Vec3::new(4., 5., 6.));
+    }
+
+    #[test]
+    fn ray_at_works() {
+        let ray = Ray::new(Vec3::new(1., 2., 3.), Vec3::new(1., 1., 1.));
+        let expected = Vec3::new(4., 5., 6.);
+
+        assert_eq!(expected, ray.at(3.));
+    }
+}
